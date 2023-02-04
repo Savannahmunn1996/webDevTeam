977d1fc4381556ede6f438b97254395bdb3d1ec8
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-03 16:29:29 -0800

    classes/constructors

---
+++ js/employee.js
@@ -0,0 +1,28 @@
+// const main = require("./index.js");
+
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  //   getRole() {
+  //     return "Employee";
+  //   }
+
+  getName() {
+    console.log(`Name: ${this.name}`);
+    return this.name;
+  }
+
+  getId() {
+    console.log(`ID: ${this.id}`);
+    return this.id;
+  }
+
+  getMail() {
+    console.log(`email: ${this.email}`);
+    return this.email;
+  }
+}
+module.exports = Employee;
---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const employee = require("./js/employee");
+// const manager = require("./js/manager.js");
+// const engineer = require("./js/engineer.js");
+// const intern = require("./js/intern.js");
+
+Sam.getName();
+Sam.getId();
+Sam.getMail();
